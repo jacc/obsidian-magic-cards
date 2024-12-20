3b6b9b6e11722afa29075acc18e54376c81e7a85
StevenStavrakis <s.a.stavrakis@gmail.com>
2024-12-20 13:16:40 -0500

    add main.ts content

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './styles.css'
+import { App, type PluginManifest, Plugin } from "obsidian"
+import type { PluginModule } from "@modules/types";
+import { ExampleModule } from "./modules/example/ExampleModule";
+
+
+export default class MyPlugin extends Plugin {
+  private modules: PluginModule[];
+
+  constructor(app: App, manifest: PluginManifest) {
+    super(app, manifest);
+    // Add modules here
+    this.modules = [
+      new ExampleModule(this)
+    ]
+  }
+
+  async onload() {
+    this.modules.forEach(module => module.onload());
+  }
+
+  onunload() {
+    this.modules.forEach(module => module.onunload());
+  }
+
+}
